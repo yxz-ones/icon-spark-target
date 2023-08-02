53cd0b767863d47d07aac2e949922286ff9e860b
yuanxiangzhu <105091686+yxz-ones@users.noreply.github.com>
2023-08-02 15:22:38 +0800

    feat(Icons):🌟 UML

---
+++ packages/icons/src/components/Uml.tsx
@@ -0,0 +1,66 @@
+import classNames from 'classnames'
+import * as React from 'react'
+import { SVGProps, memo } from 'react'
+interface IconProps extends SVGProps<SVGSVGElement> {
+  /** 图标的类名 */
+  className?: string;
+  /** 图标颜色，也可以通过"CSS"的"color"属性修改 */
+  color?: string;
+  /** 图标的尺寸 */
+  fontSize?: number | string;
+  /** 高亮色，仅双色图标有效 */
+  highlight?: string;
+  /** 是否阻止图标的轮廓宽度随图标的缩放而改变 */
+  nonScalingStroke?: boolean;
+  /** 图标轮廓的宽度，可选值为"1" "2" */
+  strokeWidth?: 1 | 2;
+  /** 图标的行内样式 */
+  style?: React.CSSProperties;
+}
+const SvgUml = memo(function ({
+  className,
+  color = 'currentColor',
+  fontSize,
+  highlight,
+  nonScalingStroke = true,
+  style,
+  strokeWidth = 2,
+  ...props
+}: IconProps) {
+  return (
+    <svg
+      {...props}
+      width="1em"
+      height="1em"
+      viewBox="0 0 16 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className={classNames(
+        'ones-icon',
+        {
+          'ones-icon-non-scaling-stroke': nonScalingStroke,
+        },
+        className
+      )}
+      style={Object.assign(
+        {
+          fontSize,
+        },
+        style
+      )}
+      strokeWidth={strokeWidth}
+    >
+      <path d="M3 10v2a1 1 0 0 0 1 1h3.5M7 8H5" stroke="#2D2D2E" />
+      <circle cx={3} cy={3} r={1.5} stroke="#2D2D2E" strokeLinecap="round" />
+      <path
+        stroke="#2D2D2E"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M7.5 11.5h7v3h-7zM7.5 6.5h7v3h-7zM3 6l2 2-2 2-2-2 2-2Z"
+      />
+      <path d="M3 6V4.5" stroke="#2D2D2E" />
+    </svg>
+  )
+})
+SvgUml.displayName = 'Uml'
+export default SvgUml
