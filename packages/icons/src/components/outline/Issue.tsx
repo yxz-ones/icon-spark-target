4cfa02772dab89c1cfe0c4c07b2870d1a5b530c9
yuanxiangzhu <105091686+yxz-ones@users.noreply.github.com>
2023-08-04 16:54:29 +0800

    feat(Icons):🌟 新增 `Issue`

---
+++ packages/icons/src/components/outline/Issue.tsx
@@ -0,0 +1,62 @@
+import classNames from 'classnames'
+import * as React from 'react'
+import { SVGProps, memo } from 'react'
+interface IconProps extends SVGProps<SVGSVGElement> {
+  /** 图标的类名 */
+  className?: string;
+  /** 图标颜色，也可以通过"CSS"的"color"属性修改 */
+  color?: string;
+  /** 图标的尺寸 */
+  fontSize?: number | string;
+  /** 高亮色，仅双色图标有效 */
+  highlight?: string;
+  /** 是否阻止图标的轮廓宽度随图标的缩放而改变 */
+  nonScalingStroke?: boolean;
+  /** 图标轮廓的宽度，可选值为"1" "2" */
+  strokeWidth?: 1 | 2;
+  /** 图标的行内样式 */
+  style?: React.CSSProperties;
+}
+const SvgIssue = memo(function ({
+  className,
+  color = 'currentColor',
+  fontSize,
+  highlight,
+  nonScalingStroke = true,
+  style,
+  strokeWidth = 2,
+  ...props
+}: IconProps) {
+  return (
+    <svg
+      {...props}
+      width="1em"
+      height="1em"
+      viewBox="0 0 16 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className={classNames(
+        'ones-icon',
+        {
+          'ones-icon-non-scaling-stroke': nonScalingStroke,
+        },
+        className
+      )}
+      style={Object.assign(
+        {
+          fontSize,
+        },
+        style
+      )}
+      strokeWidth={strokeWidth}
+    >
+      <path
+        d="M10.75 2.5h1.75a1 1 0 0 1 1 1v10a1 1 0 0 1-1 1h-9a1 1 0 0 1-1-1v-10a1 1 0 0 1 1-1h1.75M4 7.5h8M4 10h5"
+        stroke="#2D2D2E"
+      />
+      <path d="M5.5 1.5h5v2h-5v-2Z" stroke="#2D2D2E" strokeLinejoin="round" />
+    </svg>
+  )
+})
+SvgIssue.displayName = 'Issue'
+export default SvgIssue
