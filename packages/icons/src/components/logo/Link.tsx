d4a1eec5009752c3d679e34ee5b80ba6a8276480
yuanxiangzhu <105091686+yxz-ones@users.noreply.github.com>
2023-08-04 14:55:17 +0800

    feat(Icons):🌟 新增 `Link`

---
+++ packages/icons/src/components/logo/Link.tsx
@@ -0,0 +1,61 @@
+import classNames from 'classnames'
+import * as React from 'react'
+import { SVGProps, memo } from 'react'
+interface IconProps extends SVGProps<SVGSVGElement> {
+  /** 图标的类名 */
+  className?: string;
+  /** 图标颜色，也可以通过"CSS"的"color"属性修改 */
+  color?: string;
+  /** 图标的尺寸 */
+  fontSize?: number | string;
+  /** 高亮色，仅双色图标有效 */
+  highlight?: string;
+  /** 是否阻止图标的轮廓宽度随图标的缩放而改变 */
+  nonScalingStroke?: boolean;
+  /** 图标轮廓的宽度，可选值为"1" "2" */
+  strokeWidth?: 1 | 2;
+  /** 图标的行内样式 */
+  style?: React.CSSProperties;
+}
+const SvgLink = memo(function ({
+  className,
+  color = 'currentColor',
+  fontSize,
+  highlight,
+  nonScalingStroke = true,
+  style,
+  strokeWidth = 2,
+  ...props
+}: IconProps) {
+  return (
+    <svg
+      {...props}
+      width="1em"
+      height="1em"
+      viewBox="0 0 16 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className={classNames(
+        'ones-icon',
+        {
+          'ones-icon-non-scaling-stroke': nonScalingStroke,
+        },
+        className
+      )}
+      style={Object.assign(
+        {
+          fontSize,
+        },
+        style
+      )}
+      strokeWidth={strokeWidth}
+    >
+      <path
+        d="m10.531 5.468-5.063 5.064M6.943 3.57l.633-.634a3.88 3.88 0 0 1 5.487 5.486l-.633.633m-3.377 3.376-.633.633a3.88 3.88 0 1 1-5.485-5.486l.633-.633"
+        stroke="#2D2D2E"
+      />
+    </svg>
+  )
+})
+SvgLink.displayName = 'Link'
+export default SvgLink
