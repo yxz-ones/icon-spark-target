51a3d2c0bfa0a365758cdd69bb487a07cdb7e2fd
yuanxiangzhu <105091686+yxz-ones@users.noreply.github.com>
2023-08-08 01:22:07 +0800

    perf(Icons):🧊 新增 `H1` 删除 `Action`

---
+++ packages/icons/src/components/outline/H1.tsx
@@ -0,0 +1,60 @@
+import classNames from 'classnames'
+import * as React from 'react'
+import { SVGProps, memo } from 'react'
+interface IconProps extends SVGProps<SVGSVGElement> {
+  /** 图标的类名 */
+  className?: string;
+  /** 图标颜色，也可以通过"CSS"的"color"属性修改 */
+  color?: string;
+  /** 图标的尺寸 */
+  fontSize?: number | string;
+  /** 高亮色，仅双色图标有效 */
+  highlight?: string;
+  /** 是否阻止图标的轮廓宽度随图标的缩放而改变 */
+  nonScalingStroke?: boolean;
+  /** 图标轮廓的宽度，可选值为"1" "2" */
+  strokeWidth?: 1 | 2;
+  /** 图标的行内样式 */
+  style?: React.CSSProperties;
+}
+const SvgH1 = memo(function ({
+  className,
+  color = 'currentColor',
+  fontSize,
+  highlight,
+  nonScalingStroke = true,
+  style,
+  strokeWidth = 2,
+  ...props
+}: IconProps) {
+  return (
+    <svg
+      {...props}
+      width="1em"
+      height="1em"
+      viewBox="0 0 16 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className={classNames(
+        'ones-icon',
+        {
+          'ones-icon-non-scaling-stroke': nonScalingStroke,
+        },
+        className
+      )}
+      style={Object.assign(
+        {
+          fontSize,
+        },
+        style
+      )}
+      strokeWidth={strokeWidth}
+    >
+      <path d="M2.5 14V2M9.5 14V2" stroke="#2D2D2E" />
+      <path d="M12.5 14V9.5L11 10" stroke="#2D2D2E" strokeLinejoin="round" />
+      <path d="M3 8h6" stroke="#2D2D2E" />
+    </svg>
+  )
+})
+SvgH1.displayName = 'H1'
+export default SvgH1
