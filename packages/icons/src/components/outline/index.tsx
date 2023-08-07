0ad60206f302976db0b77a184ad0245d4d1a0916
yuanxiangzhu <105091686+yxz-ones@users.noreply.github.com>
2023-08-07 16:06:00 +0800

    feat(Icons):🌟 新增 `TXTFile`

--- packages/icons/src/components/outline/index.tsx
+++ packages/icons/src/components/outline/index.tsx
@@ -1,2 +1,5 @@
 export { default as Action } from './Action'
+export { default as Add } from './Add'
+export { default as TxtFile } from './TxtFile'
 export { default as Todo } from './Todo'
+export { default as ZoomOut } from './ZoomOut'
